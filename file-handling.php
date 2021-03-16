054ec532c27cdfc90e284fef7dc5940cd06b9a6d
ridwan101 <78007081+ridwan101@users.noreply.github.com>
2021-03-16 19:28:46 +0600

    Update file-handling.php

--- file-handling.php
+++ file-handling.php
@@ -1,37 +1,102 @@
+<?php
+
+if(isset($_POST['firstname']))
+{
+$data=$_POST['firstname'];
+$fp = fopen('Registration.txt', 'a');
+fwrite($fp, $data);
+fclose($fp);
+}
+
+if(isset($_POST['lastname']))
+{
+$data=$_POST['lastname'];
+$fp = fopen('Registration.txt', 'a');
+fwrite($fp, $data);
+fclose($fp);
+}
+
+if(isset($_POST['email']))
+{
+$data=$_POST['email'];
+$fp = fopen('Registration.txt', 'a');
+fwrite($fp, $data);
+fclose($fp);
+}
+
+if(isset($_POST['uname']))
+{
+$data=$_POST['uname'];
+$fp = fopen('Registration.txt', 'a');
+fwrite($fp, $data);
+fclose($fp);
+}
+
+if(isset($_POST['remail']))
+{
+$data=$_POST['remail'];
+$fp = fopen('Registration.txt', 'a');
+fwrite($fp, $data);
+fclose($fp);
+}
+
+if(isset($_POST['password']))
+{
+$data=$_POST['password'];
+$fp = fopen('Registration.txt', 'a');
+fwrite($fp, $data);
+fclose($fp);
+}
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en-US">
 <head>
-
-   <title>File handling</title>
-
- </head>
+	<title>
+		Register Form
+	</title>
+</head>
 <body>
-   <h1>PHP file Example</h1>
+<<form method="post">
+    
+    <p style="color:black;font-weight: bold;font-size: 28"> Basic Information:</p>
 
-   <?php
+    <label for="firstname">First Name:</label>
+    <input type="text" name="firstname" required>
+    <br>
 
-   $filePath="Registration.txt";
+    <label for="lastname">Last Name:</label>
+    <input type="text" name="lastname" required>
+    <br>
 
-   echo readfile($filePath);
+    <label for="gen"><b>Gender</b></label>
 
-   echo "<br>";
+    <input type="radio" id="male" name="gender" value="male">
+  <label for="male">Male</label>
+  <input type="radio" id="female" name="gender" value="female">
+  <label for="female">Female</label>
+  <input type="radio" id="other" name="gender" value="other">
+  <label for="other">Other</label>
+    <br>
 
-   echo fopen($filePath,"r");
+  <label for="email">Email:</label>
+  <input type="text" name="email" required>
+  <br>
 
-   echo "<br>";
+  <p style="color:black;font-weight: bold;font-size: 28"> User Account Information:</p>
 
-   $f =fopen($filePath,"r");
+  <label for="uname">User Name:</label>
+  <input type="text" name="username" required>
+  <br>
+  <label for="password">Password:</label>
+  <input type="password" name="password" required>
+  <br>
+  <label for="remail">Recovery Email:</label>
+  <input type="text" name="remail" required>
+  <br>
+  <input type="Reset">
+  <br>
+  <input type="submit">
 
-   echo fread($f,filesize($filePath));
-
-  $f1=fopen($filePath,"w");
-
-  echo fwrite($f1,filesize($filePath));
-
-
-
-
-   ?>
-
+</form>
 </body>
-</html>   
+</html>
